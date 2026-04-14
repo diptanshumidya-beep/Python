7d16ba95267d5df2e33cb1deef906106c9447e00
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-14 21:19:30 +0530

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,29 @@
+print("Welcome to the world of programming!")
+
+print("4")
+
+
+print("Hello\n Python\n")
+
+print("Hello",5)
+
+print("welcome to",end="*")
+
+print("Diptanshu")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
