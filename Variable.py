f540bf79d6a5172f6a80dac23207eb1de331949f
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-14 21:34:42 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,28 @@
+x = 5
+y= "John"
+
+print(x)
+
+print(y)
+
+codingal = "codingal"
+
+print(codingal)
+
+name = input("enter your name")
+
+
+print("\n Hello",name,"\nWelcome to codingal")
+print("welcome " + name)
+
+
+ViratKohli = 18
+MahendraSinghDhoni = 7
+
+print(f"sum is {ViratKohli +MahendraSinghDhoni }")
+
+PI = 3.14746981387389524568947573485734787997237848746374677464897489473274723847
+
+print(f"{PI:.10f}")
+
+
