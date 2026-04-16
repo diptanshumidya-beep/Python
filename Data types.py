bfa55f40629a889270f7604cf5b4a3ab8eec0933
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-16 21:44:31 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,32 @@
+m = 7
+
+print("type of m: ",type(m))
+
+s = 7.7
+
+print("type of m: ",type(s))
+
+
+msd = "Mahendra Singh Dhoni"
+
+print("type of msd: ",type(msd))
+
+
+MSD_GOAT = True 
+
+print("type of MSD_GOAT: ",type(MSD_GOAT))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
