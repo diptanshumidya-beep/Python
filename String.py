2051cbb4c9b708cc573e7ee5fae93e1f5d2d976b
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-19 16:29:53 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,27 @@
+text = input("Enter your text")
+
+print (text.lower())
+
+print (text.upper())
+
+text2 = ["Apple","Mango","Bannana"]
+
+text3 =  ("#".join(text2))
+
+
+print (text3)
+
+
+
+text4 = "TODAY IS A GODD DAY"
+
+print (text4.split())
+
+text5 =  "I like to play football"
+
+print (text5.replace("football","Cricket"))
+
+text6 = "Today is our match"
+
+
+print(text.find("match"))
