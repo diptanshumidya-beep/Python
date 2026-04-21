0aab727347c1fd054edaf2dc4a14f49f166a3155
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-21 21:42:54 +0530

    Add files via upload

---
+++ Coint Notes.py
@@ -0,0 +1,22 @@
+
+
+Amount = int(input("Please enter your bill ammount :"))
+
+
+Note1 = Amount//100
+
+Note2 = (Amount%100)//50
+
+Note3 = ((Amount%100)%50)//10
+
+print ("notes of 100 rupee", Note1)
+
+print ("notes of 50 rupee", Note2)
+
+
+print ("notes of 10rupee", Note3)
+
+
+
+
+
---
+++ Perecentage.py
@@ -0,0 +1,33 @@
+
+print("Enter your marks scored in all subjects out of 80")
+
+
+
+Maths = int(input("Maths :"))
+
+English = int(input("English :"))
+
+Physics = int(input("Physics :"))
+
+
+
+Chemistry = int(input("Chemistry :"))
+
+
+Biology = int(input("Biology  :"))
+
+
+
+Sanskrit = int(input("Sanskrit :"))
+
+
+
+sum = Maths + English +Physics  + Chemistry+Biology + Sanskrit
+print ("Sum of all marks scored in exam is :",sum)
+
+
+pER = (sum/480)*100
+
+print ("The percentage of your eaxams is :",pER,"%")
+
+
