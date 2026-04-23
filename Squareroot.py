d2b92e7818b12c81a4c01c582444ec83fed584ef
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-23 21:57:59 +0530

    Add files via upload

---
+++ Squareroot.py
@@ -0,0 +1,11 @@
+x =  int(input("Enter your input for square root"))
+
+print(x**0.5)
+
+
+
+y = (input("Enter your number"))
+
+z = len(y)
+
+print("Your number is",z,"consits of digits")
---
+++ Weather.py
@@ -0,0 +1,10 @@
+Weather = int(input("Enter your locations temparature in degree centrigrade"))
+
+if Weather<15:
+    print("It is a cold day. Wear a jacket")
+
+if (15<Weather<25) :
+    print("It is a normal Day. Wear casual")
+
+if (Weather>37) :
+    print("It is a hot Day. Wear light-coloured clothes")
