35f2cc01ac1a370d37a2614eafc72c718bfe5292
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-30 19:55:14 +0530

    Add files via upload

---
+++ Power Calculator.py
@@ -0,0 +1,36 @@
+base = int(input("Enter your base for ppowering"))
+
+power = int(input("Enter your power for poweering"))
+
+exponenetation = 1
+
+for i in range (power):
+    exponenetation = exponenetation*base
+    
+print ("Your exponentation value is",exponenetation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
