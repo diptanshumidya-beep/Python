5b49f1780c9e6923a7adb6afce970411f2b4e413
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-04-30 19:52:56 +0530

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,31 @@
+string = input("Please enter your own string:")
+
+
+string2 = ('')
+
+for i in string:
+    string2 = string2+i
+
+print("THE ORIGINAL STRING IS",string)
+
+print("THE REVERSED STRING IS",string2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Wole numbers.py
@@ -0,0 +1,31 @@
+inputt = int(input("eNTER THE NUMBER WHOSE SUM YOU WANT TOD DO :"))
+
+SUM = 0
+
+
+for i in range(1, inputt+1):
+    SUM = SUM+i
+
+print ("Sum is",SUM)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
