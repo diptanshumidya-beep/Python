0d4d53b72a1ecd09a023b01c154c575c48faf633
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-06-11 11:33:13 +0530

    Add files via upload

---
+++ Natural Numbers.py
@@ -0,0 +1,34 @@
+number = int(input("Enter a number for addition"))
+
+sum = 0
+
+i = 1
+
+while i<=number:
+    sum = sum + i
+    i = i+1
+
+print("Your sum is ", sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ infinite.py
@@ -0,0 +1,5 @@
+
+i = 0
+
+while i>=1:
+    print("I will last for forever hahhahahhhahhahhahhahhahahhahahhahhaha!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
