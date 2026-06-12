8425b05a5d49df87a420f6c8f42ffd1522a05f6c
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-06-12 20:21:02 +0530

    Add files via upload

---
+++ pi om baba.py
@@ -0,0 +1,26 @@
+def circumference (Radius):
+    return (3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679*2*Radius)
+
+Radu =  (int(input("Enter the radius")))
+
+print (circumference(Radu))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
