807d21be49806bb508fe54d65cc2d354c9e6405f
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-06-23 15:49:32 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,36 @@
+
+
+try:
+    Age = int(input("Enter your age"))
+    odd =  Age%2
+    if odd ==  0:
+         print("Your age is even")
+    else:
+        print("Your age is odd")
+
+
+
+
+
+
+
+except ValueError:
+    print("Please enter a number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
