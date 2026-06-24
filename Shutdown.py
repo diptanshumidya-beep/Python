98043393648926943de4083347e692f43dfa3cb1
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-06-24 19:11:10 +0530

    Add files via upload

---
+++ Shutdown.py
@@ -0,0 +1,19 @@
+def shutdown ():
+    return
+    
+print("This is a shut down function ")
+
+print ((input("Do you really want to shut down? Y/N")).upper())
+if "Y":
+    print("SHUT DOWN HAS BEEN CONFIRMED")
+    shutdown()
+
+else:
+    print("Shutdown has not been confirmed")
+
+
+
+
+
+
+
