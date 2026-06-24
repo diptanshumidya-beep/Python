0a5c685686892aa4828b1cf73dc969e1bf92cbf7
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-06-24 18:51:58 +0530

    Add files via upload

---
+++ Due Ammount.py
@@ -0,0 +1,18 @@
+
+def Due (bill,money):
+    return (bill - money)
+
+print("Enter values like ....rupees")
+bill = int(input("Enter the bill ammount"))
+
+money = int(input("Enter the money ammount you are willing to give "))
+
+print (Due(bill,money))
+
+
+
+
+
+
+
+
