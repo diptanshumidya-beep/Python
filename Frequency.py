4434b0814c718e659d70f7df6668aa1ecd74f8ec
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-07-09 21:58:19 +0530

    Add files via upload

---
+++ Frequency.py
@@ -0,0 +1,19 @@
+test_dict = {'Coding' : 2, 'is':2, 'best':2,'for': 2, "coding":1 }
+
+print('The original dictionary is'+ str(test_dict))
+
+K= 2
+
+res =0
+for key in test_dict:
+    if test_dict  [key] == K:
+        res = res+1
+
+print(str(res))
+
+
+
+
+
+
+
---
+++ country.py
@@ -0,0 +1,14 @@
+country_code = { 
+    'India': '0091',
+    'Australia':'0025',
+    'Nepal':"0007869"
+}
+
+print("Country code for India -")
+print(country_code.get("India",'Not Found'))
+
+
+
+print("Country code for Japan -")
+print(country_code.get("Japan",'Not Found'))
+
