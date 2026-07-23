ce72f97969505d698e59851e050fcb9e0b6a516f
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-07-23 21:45:46 +0530

    Add files via upload

---
+++ Phyton Coding TEST 3.py
@@ -0,0 +1,24 @@
+Alice = 20
+
+Bob = 40
+
+Cat = 60
+
+Fan = 75
+
+DIPUANNA = 80
+
+
+YOU = 45
+
+WHEY = 68
+
+RAM = 76
+
+JODHU = 39
+
+
+Average = ((Alice + Bob+  Cat + Fan + DIPUANNA + YOU + WHEY + RAM +JODHU) /9)
+
+
+print (Average)
