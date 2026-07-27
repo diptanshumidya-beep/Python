2b7ea71d206d9409111f5e1589937d5cfeb5bb0d
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-07-27 21:51:57 +0530

    Add files via upload

---
+++ Parrot.py
@@ -0,0 +1,24 @@
+class Parrot:
+
+    species = "bird"
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
+
+
+blu = Parrot("Blu",10)
+
+woo = Parrot("wOO",15)
+
+print ("Blu is a {}".format (blu.species))
+
+print ("WOO is a {}".format (woo.species))
+
+
+print("{} is {} years old".format(blu.name,blu.age))
+
+print("{} is {} years old".format(woo.name,woo.age))
+
+
+
---
+++ Veichle.py
@@ -0,0 +1,25 @@
+class Vehicle:
+
+    def __init__(self,max_speed,mileage):
+
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+modelx = Vehicle(240,18)
+
+
+print("ModelX Max Speed",modelx.max_speed)
+
+print("Modelx MILEAGE",modelx.mileage)
+
+
+
+
+
+
+
+
+
+
+
