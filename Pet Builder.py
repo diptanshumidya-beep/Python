b572013ee6ab76ddd0b8ccd756d1757c323550a2
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-07-28 19:02:54 +0530

    Add files via upload

---
+++ Pet Builder.py
@@ -0,0 +1,15 @@
+class pet:
+    def __init__( self,Namee,Agee,Typee):
+        self.Namee = Namee
+        self.Agee = Agee
+        self.Typee = Typee
+
+
+Pet = pet("BOB",67,"DOG")
+
+
+print("Pet's Name is ",Pet.Namee)
+
+print("Pet's AGE is ",Pet.Agee)
+
+print("Pet's Type is ",Pet.Typee)
