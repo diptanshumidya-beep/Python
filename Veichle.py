ad69bf8c987cfe4cbc3cad2e9d13695260c308cb
diptanshumidya-beep <diptanshumidya@gmail.com>
2026-08-31 20:51:03 +0530

    Add files via upload

--- Veichle.py
+++ Veichle.py
@@ -1,25 +1,50 @@
-class Vehicle:
+class Vhicle:
 
-    def __init__(self,max_speed,mileage):
+    def __init__(self, company, engine_type):
+        self.company = company
+        self.engine_type = engine_type
 
-        self.max_speed = max_speed
+    def prrrint(self):
+        print("Company:", self.company)
+        print("Engine type:", self.engine_type)
+
+
+class Kid(Vhicle):
+
+    def __init__(self, mileage, speed, colour, company, engine_type):
+       
+
+       
         self.mileage = mileage
+        self.speed = speed
+        self.colour = colour
+        super().__init__(company, engine_type)
+
+    def print_details(self):
+        print("Mileage:", self.mileage)
+        print("Speed:", self.speed)
+        print("Colour:", self.colour)
+        print("ENGINE TYPE", self.engine_type)
+        print("company", self.company)
+        
+       
+        super().prrrint()
 
 
-modelx = Vehicle(240,18)
+parent = Vhicle("Volkswagen", "V12")
+parent.prrrint()
 
+print()
 
-print("ModelX Max Speed",modelx.max_speed)
+child = Kid(
+    150,
+    300,
+    "Orange",
+    "Volkswagen",
+    "V12")
+    
 
-print("Modelx MILEAGE",modelx.mileage)
+child.print_details()
 
-
-
-
-
-
-
-
-
-
-
+print()
+print("Is Kid a subclass of Vehicle?", issubclass(Kid, Vhicle))
